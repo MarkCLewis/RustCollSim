9f82032cbed562f8aa4a31d32b6a50707f2c6e4f
Gracen Hoyle <ghoyle@trinity.edu>
2020-09-11 13:26:06 -0500

    Pushed meeting 9/11 code

--- src/gh-main.rs
+++ src/gh-main.rs
@@ -1,52 +1,78 @@
-struct Position {
-    x: f32,
-    y: f32,
-    z: f32
-}
+// struct Position {
+//     x: f32,
+//     y: f32,
+//     z: f32
+// }
 
-struct Velocity {
-    x: f32,
-    y: f32,
-    z: f32
-}
+// struct Velocity {
+//     x: f32,
+//     y: f32,
+//     z: f32
+// }
 
-struct A {
-    a: f32,
-    b: f32,
-    c: f32
+struct Vector {
+    x: f64,
+    y: f64,
+    z: f64
 }
 
 struct Particle {
-    pos: Position,
-    vel: Velocity,
-    avect: A
+    pos: Vector,
+    vel: Vector,
+    acc: Vector,
+    mass: f64
 }
 
 fn main() {
     println!("Welcome to the Kick Step Integrator!");
-    let px: f32 = 0.0;
-    let py: f32 = 0.0;
-    let pz: f32 = 0.0;
-    let vx: f32 = 0.0;
-    let vy: f32 = 0.0;
-    let vz: f32 = 0.0;
-    let a: f32 = 0.0;
-    let b: f32 = 0.0;
-    let c: f32 = 0.0;
+    let px: f64 = 0.0;
+    let py: f64 = 0.0;
+    let pz: f64 = 0.0;
+    let vx: f64 = 0.0;
+    let vy: f64 = 0.0;
+    let vz: f64 = 0.0;
+    let ax: f64 = 0.0;
+    let ay: f64 = 0.0;
+    let az: f64 = 0.0;
 
-    let particle = Particle {pos: Position{x: px, y: py, z: pz}, 
-                             vel: Velocity{x: vx, y: vy, z: vz }, 
-                             avect: A{a: a, b: b, c: c} };
+    let earthM: f64 = 1e-10;
+    let sunM: f64 = 1;
+
+    let eParticle = Particle {pos: Vector{x: px, y: py, z: pz,}, 
+                                vel: Vector{x: vx, y: vy, z: vz }, 
+                                avect: Vector{a: ax, b: ay, c: az},
+                                mass: earthM };
+
+    let sParticle = Particle {pos: Vector{x: px, y: py, z: pz,}, 
+                                vel: Vector{x: vx, y: vy, z: vz }, 
+                                avect: Vector{a: ax, b: ay, c: az},
+                                mass: sunM };
+
+    // array of particles
+    // outer for loop (timestep)
+        // doubly nested loop that calculates acceleration btwn two particles
+        // loop applies accelerations, then velocities (outside)
 }
 
-fn force(g: i32, m1: i32, m2: i32, r: i32) {
-    ((-1 * g) * (m1 * m2)) / (r*r);
+fn force(mass1: f64, mass2: f64, r: f64) {
+    //vector quantities between two bodies
+ 
+    // calculate distance, use that
+    (-1 * (m1 * m2)) / (r*r*r);    // multiply by seperation between them (only vector), divide by r^3
 }
-fn velocity(v: i32, dt: i32, a: i32) {
-    v + dt * a;
+fn velocity (par: Particle) { // give back particle? or pass in mut
+    //v + dt * a;
+    particle.vel.x = particle.vel.x + timestep * particle.acc.x;
+    particle.vel.y = particle.vel.y + timestep * particle.acc.y;
+    particle.vel.z = particle.vel.z + timestep * particle.acc.z;
+    // done to x, y, z separately @ v
 }
-fn updatePosition(x: i32, dt: i32, v: i32) {
-    x + dt * v; 
+
+fn updatePosition (par: Particle, timestep: f64) {
+    particle.pos.x = particle.pos.x + timestep * particle.vel.x;
+    particle.pos.y = particle.pos.y + timestep * particle.vel.y;
+    particle.pos.z = particle.pos.z + timestep * particle.vel.z;
+    // done to x, y, z separately @ x 
 }
 
 // Take all particles
