a39ebe6f5e5c4cbf81ad8f7c2995d6d8e47417fa
Jonathan Rotter <hydrogen31415@gmail.com>
2020-09-25 17:41:36 +0200

    collision but drag is broken

--- src/data.rs
+++ src/data.rs
@@ -2,9 +2,12 @@
 pub mod data {
 
     pub const PI: f64 = 3.141592653589793;
-    pub const K: f64 = 1.0;
+    pub const K_DEFAULT: f64 =  1e11;
+    pub const DRAG_DEFAULT: f64 = 1e3;
 
     pub mod basic {
+        use std::ops::{Mul, Div, Sub};
+
         fn square(x: f64) -> f64 { x * x }
 
         // x, y, z for all
@@ -36,6 +39,22 @@ pub mod data {
             //     Displacement(self.0, self.1, self.2)
             // }
         }
+
+        impl Div<f64> for Displacement {
+            type Output = Self;
+
+            fn div(self, f: f64) -> Displacement {
+                Displacement(self.0/f, self.1/f, self.2/f)
+            }
+        }
+
+        impl Mul<f64> for Displacement {
+            type Output = Self;
+
+            fn mul(self, f: f64) -> Displacement {
+                Displacement(self.0*f, self.1*f, self.2*f)
+            }
+        }
     
         impl Velocity {
             pub fn multiply_integrate(&self, t: f64) -> Displacement {
@@ -48,6 +67,30 @@ pub mod data {
     
             pub fn copy(&self) -> Velocity {
                 Velocity(self.0, self.1, self.2)
+            }
+        }
+
+        impl Div<f64> for Velocity {
+            type Output = Self;
+
+            fn div(self, f: f64) -> Velocity {
+                Velocity(self.0/f, self.1/f, self.2/f)
+            }
+        }
+
+        impl Mul<f64> for Velocity {
+            type Output = Self;
+
+            fn mul(self, f: f64) -> Velocity {
+                Velocity(self.0*f, self.1*f, self.2*f)
+            }
+        }
+
+        impl Sub for Velocity {
+            type Output = Self;
+
+            fn sub(self, other: Velocity) -> Velocity {
+                Velocity(self.0 - other.0, self.1 - other.1, self.2 - other.2)
             }
         }
     
@@ -72,9 +115,10 @@ pub mod data {
     pub mod systems {
         use itertools::izip;
         use super::basic::*;
-        use super::K;
+        use std::ops::{AddAssign, Div, Mul, Add};
 
-        struct State(pub Displacement, pub Velocity);
+        #[derive(Clone, Copy)]
+        pub struct State(pub Displacement, pub Velocity);
 
         struct Derivative(pub Velocity, pub Acceleration);
 
@@ -86,35 +130,82 @@ pub mod data {
 
         #[derive(Clone, Copy)]
         pub struct Particle {
-            pub location: Displacement,
-            velocity: Velocity,
+            pub state: State,
             mass: f64,
             size: f64
+        }
+
+        impl AddAssign for State {        
+            fn add_assign(&mut self, other: Self) {
+                *self = Self(self.0.addition(&other.0), self.1.addition(&other.1));
+            }
+        }
+
+        impl Add for State {
+            type Output = Self;
+
+            fn add(self, other: State) -> Self::Output {
+                State(self.0.addition(&other.0), self.1.addition(&other.1))
+            }
+        }
+
+        impl Div<f64> for State {
+            type Output = Self;
+
+            fn div(self, other: f64) -> Self::Output {
+                State(self.0 / other, self.1 / other)
+            }
+        }
+
+        impl Mul<f64> for State {
+            type Output = Self;
+
+            fn mul(self, other: f64) -> Self::Output {
+                State(self.0 * other, self.1 * other)
+            }
         }
     
         impl Particle {
             pub fn print_out(&self) {
-                let Displacement(x, y, z) = self.location;
-                let Velocity(vx, vy, vz) = self.velocity;
+                let Displacement(x, y, z) = self.state.0;
+                let Velocity(vx, vy, vz) = self.state.1;
                 println!("Position = <{}, {}, {}> Velocity = <{}, {}, {}>", x, y, z, vx, vy, vz);
             }
         }
 
         pub fn build_particle(x: f64, y: f64, z: f64, vx: f64, vy: f64, vz: f64, mass: f64, size: f64) -> Particle {
             return Particle {
-                location: Displacement(x, y, z),
-                velocity: Velocity(vx, vy, vz),
+                state: State(
+                    Displacement(x, y, z),
+                    Velocity(vx, vy, vz)
+                ),
                 mass: mass,
                 size: size
             }
         }
 
-        pub struct System(pub Vec<Particle>);
+        pub struct System {
+            pub state: Vec<Particle>,
+            pub k: f64,
+            pub drag: f64
+        }
+
+        impl AddAssign<&Vec<State>> for System {
+            fn add_assign(&mut self, state: &Vec<State>) {
+                for (p, s) in izip!(self.state.iter_mut(), state.iter()) {
+                    p.state += *s;
+                }
+            }
+        }
 
         impl System {
 
+            pub fn get_velocity(&self, index: usize) -> Velocity {
+                self.state[index].state.1
+            }
+
             fn derivative(&self) -> Vec<Derivative> {
-                let sys = &self.0;
+                let sys = &self.state;
 
                 let mut deriv: Vec<Derivative> = Vec::new();
             
@@ -128,7 +219,7 @@ pub mod data {
                         if i != j {
                             let other = &sys[j];
             
-                            let d_sq: f64 = this.location.distance_sq(&other.location);
+                            let d_sq: f64 = this.state.0.distance_sq(&other.state.0);
                             let d: f64 = d_sq.sqrt();
             
                             // a = g * m / r^2
@@ -137,7 +228,7 @@ pub mod data {
             
                             let acc_scalar: f64 = other.mass / d_sq; // magnitude of a
             
-                            let c: Displacement = other.location.subtraction(&this.location).multiply(1.0/d);
+                            let c: Displacement = other.state.0.subtraction(&this.state.0).multiply(1.0/d);
                             // now c is a vector Displacementing to b from this
                             // c is displacement unit vector
             
@@ -155,47 +246,55 @@ pub mod data {
                             let delta_x: f64 = d - (this.size + other.size);
                             if delta_x < 0.0 {
                                 // intersection!
-            
+                                //let tmp = self.get_velocity(0);
+                                //eprintln!("Collision: v=<{:.3e}, {:.3e}, {:.3e}>", tmp.0, tmp.1, tmp.2);
+
+
+                                let rel: Velocity = other.state.1 -this.state.1;
+
+                                let v_mag: f64 = rel.0 * c.0 + rel.1 * c.1 + rel.2 * c.2;
+                                //rel.dot(c);
+
                                 // in dir -c (away from other object)
-                                let a: f64 = -K * delta_x / this.mass;
+                                let a: f64 = (-self.k * -delta_x + self.drag * v_mag) / this.mass;
+                                
+                                let ca = c * a;
+                                acc_vector = acc_vector.addition(&Acceleration(ca.0, ca.1, ca.2));
             
-                                let c = c.multiply(a);
+                                // // in dir -c (away from other object)
+                                // let a: f64 = -self.k * delta_x / this.mass;
             
-                                acc_vector = acc_vector.addition(&Acceleration(c.0, c.1, c.2));
+                                // let c = c.multiply(a);
+            
+                                // acc_vector = acc_vector.addition(&Acceleration(c.0, c.1, c.2));
                             }
                         }
                     }
             
             
-                    deriv.push(Derivative(this.velocity.copy(), acc_vector));
+                    deriv.push(Derivative(this.state.1.copy(), acc_vector));
                 } 
             
                 return deriv;
             }
 
-            fn add_state_to_self(&mut self, state: Vec<State>) {
-                for (p, s) in izip!(self.0.iter_mut(), state.iter()) {
-
-                }
-            }
-
             pub fn push(&mut self, p: Particle) {
-                self.0.push(p);
+                self.state.push(p);
             }
 
             pub fn add_body(&mut self, x: f64, y: f64, z: f64, vx: f64, vy: f64, vz: f64, mass: f64, size: f64) {
-                self.0.push(build_particle(x, y, z, vx, vy, vz, mass, size));
+                self.state.push(build_particle(x, y, z, vx, vy, vz, mass, size));
             }
 
             pub fn len(&self) -> usize {
-                return self.0.len();
+                return self.state.len();
             }
 
             pub fn kick_step(&mut self, h: f64) {
             
                 let deriv: Vec<Derivative> = self.derivative();
 
-                let sys = &mut self.0;
+                let sys = &mut self.state;
 
                 if sys.len() != deriv.len() {
                     panic!("length mismatch!");
@@ -209,90 +308,136 @@ pub mod data {
                     let vel = vel.addition(&acc.multiply_integrate(h));
 
                     // step
-                    s.velocity = s.velocity.addition(&acc.multiply_integrate(h));
-                    s.location = s.location.addition(&vel.multiply_integrate(h));
+                    s.state.1 = s.state.1.addition(&acc.multiply_integrate(h));
+                    s.state.0 = s.state.0.addition(&vel.multiply_integrate(h));
                 }
 
             }
 
-            // pub fn rk4(&mut self, h: f64) {
-            //     /*
-            //      * Runge-Kutta
-            //      * 
-            //      * y' = F(x, y)
-            //      * 
-            //      * y_n+1 = y_n + 1/6 * (k1 + 2k2 + 2k3 + k4) + O(h^5)
-            //      * 
-            //      * k1 = hF(xn, yn)
-            //      * k2 = hF(xn + h/2, yn + k1/2)
-            //      * k3 = hF(xn + h/2, yn + k2/2)
-            //      * k4 = hF(xn + h, yn + k3)
-            //      * 
-            //      * y is position & velocity
-            //      * y' is velocity & acceleration
-            //      * 
-            //      * idk what to do with x for now
-            //      */
+            pub fn rk4(&mut self, h: f64) {
+                /*
+                 * Runge-Kutta
+                 * 
+                 * y' = F(x, y)
+                 * 
+                 * y_n+1 = y_n + 1/6 * (k1 + 2k2 + 2k3 + k4) + O(h^5)
+                 * 
+                 * k1 = hF(xn, yn)
+                 * k2 = hF(xn + h/2, yn + k1/2)
+                 * k3 = hF(xn + h/2, yn + k2/2)
+                 * k4 = hF(xn + h, yn + k3)
+                 * 
+                 * y is position & velocity
+                 * y' is velocity & acceleration
+                 * 
+                 * idk what to do with x for now
+                 */
+
+                /*
+                 * make a copy of sys
+                 * final_k = vec<state>
+                 * k1 = iter derivative() * h
+                 * loop over k1
+                 *      add k1 /6 to final_k
+                 *      add k1 /2 to sys
+                 * k2 = iter derivative() * h
+                 * recopy sys
+                 * loop over k2
+                 *      add k2 * 2/6 to final_k
+                 *      add k2 /2 to sys
+                 * k3 = iter derivative() * h
+                 * recopy sys
+                 * loop over k3
+                 *      add k3 * 2/6 to final_k
+                 *      add k3 to sys
+                 * k4 = iter derivative() * h
+                 * loop over k4
+                 *      add k4 /6 to final_k
+                 * 
+                 * 
+                 */
                 
-            //     let mut sysOld = System(self.0.clone());
-            //     //Vector oldState(data);
+                let mut sys_copy = build_system2(self.k, self.drag, self.state.clone());
+                //System(self.0.clone());
+                //Vector oldState(data);
             
-            //     // k1 = hF(xn, yn)
-            //     // Vector k1;
-            //     // derivative(k1);
-            //     let k1: Vec<State> = self.derivative().into_iter().map(|x| x.multiply_integrate(h)).collect();
-            //     // k1 *= h;
+                // k1 = hF(xn, yn)
+                // Vector k1;
+                // derivative(k1);
+                let k1: Vec<State> = sys_copy.derivative().into_iter().map(|x| x.multiply_integrate(h)).collect();
+                // k1 *= h;
             
-            //     // k2 = hF(xn + h/2, yn + k1/2)
+                // k2 = hF(xn + h/2, yn + k1/2)
+                let k1_tmp = k1.clone();
+                let halved: Vec<State> = k1_tmp.into_iter().map(|x| x / 2.0).collect();
+                sys_copy += &halved;
+                // (*this) += k1 / 2;
 
 
-            //     (*this) += k1 / 2;
-            
-            //     Vector k2;
-            //     derivative(k2);
-            //     k2 *= h;
-            
-            //     data = oldState;
-            //     // k3 = hF(xn + h/2, yn + k2/2)
+
+                let k2: Vec<State> = self.derivative().into_iter().map(|x| x.multiply_integrate(h)).collect();
+                // Vector k2;
+                // derivative(k2);
+                // k2 *= h;
+
+                sys_copy = build_system2(self.k, self.drag, self.state.clone());
+                // data = oldState;
+                // // k3 = hF(xn + h/2, yn + k2/2)
                 
-            //     (*this) += k2 / 2;
+                // (*this) += k2 / 2;
+                let k2_tmp = k2.clone();
+                let halved: Vec<State> = k2_tmp.into_iter().map(|x| x / 2.0).collect();
+                sys_copy += &halved;
             
-            //     Vector k3;
-            //     derivative(k3);
-            //     k3 *= h;
+                // Vector k3;
+                // derivative(k3);
+                // k3 *= h;
+                let k3: Vec<State> = sys_copy.derivative().into_iter().map(|x| x.multiply_integrate(h)).collect();
+
+                sys_copy = build_system2(self.k, self.drag, self.state.clone());                // data = oldState;
+                // // k4 = hF(xn + h, yn + k3)
             
-            //     data = oldState;
-            //     // k4 = hF(xn + h, yn + k3)
+                // (*this) += k3;
+
+                sys_copy += &k3;
             
-            //     (*this) += k3;
+                // Vector k4;
+                // derivative(k4);
+                // k4 *= h;
+                let k4: Vec<State> = self.derivative().into_iter().map(|x| x.multiply_integrate(h)).collect();
+                
+                // sys_copy = System(self.0.clone());
+                // data = oldState;
+                // // 1/6 * (k1 + 2k2 + 2k3 + k4)
+
+                let mut final_k: Vec<State> = Vec::new();
             
-            //     Vector k4;
-            //     derivative(k4);
-            //     k4 *= h;
+                // Vector K(data.size());
             
-            //     data = oldState;
-            //     // 1/6 * (k1 + 2k2 + 2k3 + k4)
+                // K += k1;
+                
             
-            //     Vector K(data.size());
+                // k2 *= 2;
+                // K += k2;
+
             
-            //     K += k1;
+                // k3 *= 2;
+                // K += k3;
             
-            //     k2 *= 2;
-            //     K += k2;
+                // K += k4;
+
+                for i in 0..k1.len() {
+                    final_k.push( ( k1[i] + k2[i] * 2. + k3[i] * 2. + k4[i] ) / 6.0 );
+                }
             
-            //     k3 *= 2;
-            //     K += k3;
+                // K *= 1.0/6;
             
-            //     K += k4;
-            
-            //     K *= 1.0/6;
-            
-            //     (*this) += K;
-            
-            // }
+                // (*this) += K;
+                *self += &final_k;
+            }
 
             pub fn print_out(&self) {
-                let sys = &self.0;
+                let sys = &self.state;
                 
                 println!("[");
             
@@ -305,10 +450,26 @@ pub mod data {
             }
         }
 
-        
+        pub fn build_system(k: f64, drag: f64) -> System {
+            let k = if k < 0.0 { super::K_DEFAULT } else { k };
+            let drag = if drag < 0.0 { super::DRAG_DEFAULT } else { drag };
 
-        pub fn build_system() -> System {
-            return System(Vec::new());
+            return System {
+                state: Vec::new(),
+                k: k,
+                drag: drag
+            };
+        }
+
+        pub fn build_system2(k: f64, drag: f64, state: Vec<Particle>) -> System {
+            let k = if k < 0.0 { super::K_DEFAULT } else { k };
+            let drag = if drag < 0.0 { super::DRAG_DEFAULT } else { drag };
+
+            return System {
+                state: state,
+                k: k,
+                drag: drag
+            };
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use data::data::PI;
 fn test_group_2_kick_step_1() {
     let h: f64 = 1e-3;
 
-    let mut sys = systems::build_system();
+    let mut sys = systems::build_system(-1., -1.);
 
     sys.push(systems::build_particle(0.,0.,0.,0.,0.,0., 1.0, 1e-7));
     sys.push(systems::build_particle(1.,0.,0.,0.,1.,0.,1e-10, 1e-7));
@@ -29,10 +29,32 @@ fn test_group_2_kick_step_1() {
     sys.print_out();
 }
 
+fn test_group_3_rk4_1() {
+    let h: f64 = 1e-3;
+
+    let mut sys = systems::build_system(-1., -1.);
+
+    sys.push(systems::build_particle(0.,0.,0.,0.,0.,0., 1.0, 1e-7));
+    sys.push(systems::build_particle(1.,0.,0.,0.,1.,0.,1e-10, 1e-7));
+
+    println!("N-body simulation");
+    println!("Number of bodies: {}" , sys.len());
+
+    let mut i: f64 = 0.0;
+    while i < 2.0*PI {
+        sys.rk4(h);
+
+        i += h;
+    }
+
+    println!("Final Result:");
+    sys.print_out();
+}
+
 fn test_group_5_graphics(orbits: i32) {
     let h: f64 = 1e-2;
 
-    let mut sys = systems::build_system();
+    let mut sys = systems::build_system(-1., -1.);
 
     sys.add_body(0., 0., 0., 0., 0., 0., 1., 0.00465479256);
     sys.add_body(1., 0., 0., 0., 1., 0., 1e-8, 4.25879793e-5);
@@ -57,14 +79,14 @@ fn test_group_5_graphics(orbits: i32) {
     let mut i: f64 = 0.0;
     while i < orbits as f64 * 2.0 * PI {
 
-        for (c, p) in sys.0.iter().enumerate() {
-            g.draw_point(p.location.0, p.location.1, '.' as u64, (c % 4) as i16);
+        for (c, p) in sys.state.iter().enumerate() {
+            g.draw_point(p.state.0.0, p.state.0.1, '.' as u64, (c % 4) as i16);
         }
 
         sys.kick_step(h);
 
-        for (c, p) in sys.0.iter().enumerate() {
-            g.draw_point(p.location.0, p.location.1, 'o' as u64, (c % 4) as i16);
+        for (c, p) in sys.state.iter().enumerate() {
+            g.draw_point(p.state.0.0, p.state.0.1, 'o' as u64, (c % 4) as i16);
 
         }
         g.refresh();
@@ -80,7 +102,7 @@ fn test_group_5_graphics(orbits: i32) {
 fn test_group_6_collision() {
     let h = 1e-2;
 
-    let mut sys = systems::build_system();
+    let mut sys = systems::build_system(-1., -1.);
 
     sys.add_body(-1e-1, 0., 0., 0., 0., 0., 1e-2, 4.25879793e-5);
     sys.add_body(1e-1, 0., 0., 0., 0., 0., 1e-2, 4.25879793e-5);
@@ -96,11 +118,6 @@ fn test_group_6_collision() {
         min = g.get_max_y();
     }
 
-    let mut min: i32 = g.get_max_x();
-    if g.get_max_y() < min {
-        min = g.get_max_y();
-    }
-
     let factor: f64 = min as f64 / (1e-0 * 2.5);
 
     g.set_scale_factor(factor);
@@ -109,14 +126,14 @@ fn test_group_6_collision() {
     while i < 100. * PI * 2. as f64 * 2.0 * PI {
         //clear();
 
-        for (c, p) in sys.0.iter().enumerate() {
-            g.draw_point(p.location.0, p.location.1, '.' as u64, (c % 4) as i16);
+        for (c, p) in sys.state.iter().enumerate() {
+            g.draw_point(p.state.0.0, p.state.0.1, '.' as u64, (c % 4) as i16);
         }
 
-        sys.kick_step(h); // uses rk4 but i don't have that rn
+        sys.rk4(h);
 
-        for (c, p) in sys.0.iter().enumerate() {
-            g.draw_point(p.location.0, p.location.1, 'o' as u64, (c % 4) as i16);
+        for (c, p) in sys.state.iter().enumerate() {
+            g.draw_point(p.state.0.0, p.state.0.1, 'o' as u64, (c % 4) as i16);
 
         }
         g.refresh();
@@ -129,11 +146,131 @@ fn test_group_6_collision() {
     g.end();
 }
 
+pub fn collision(k: f64,drag: f64) {
+    let mut g = graphics::build_graphics();
+    g.init();
+
+    let mut min: i32 = g.get_max_x();
+    if g.get_max_y() < min {
+        min = g.get_max_y();
+    }
+
+    let factor: f64 = min as f64 / (1e-6 * 2.5);
+
+    g.set_scale_factor(factor);
+
+    // std::cerr << "Survived g.init\n";
+
+    let h: f64 = 1e-5;
+    //std::cerr << "> k = " << k << ", drag = " << drag << '\n';
+    let mut sys = systems::build_system(k, drag);
+
+    // State sys(h, 0, 0, k, drag);
+
+    let r: f64 = 1e-7; // 130000 km
+
+    // 2g/cm^3
+
+    // saturns mass = 5.683e26 kg
+    //              = 5.683e29 g
+    //
+    // radius       = 1.3e5 km
+    //              = 1.3e8 m
+    //              = 1.3e10 cm
+    //
+    //                          (1.3e10)^3
+    // so conversion factor = --------------
+    //                           5.683e29
+
+    let rho: f64 = 7.7; //129000; sat mass/ ring radius^3
+    let mass: f64 = 4.0/3.0 * 3.14159 * r * r * r * rho;
+
+    sys.add_body(-2. * r, 0., 0., 2e-6, 0., 0., mass, r);
+    sys.add_body(2. * r, 0., 0., -2e-6, 0., 0., mass, r);
+
+
+
+    //sys.addBody(0, 35e-4, 0, 5, 0, 0, 1e-2, 4.25879793e-4);
+    //sys.addBody(0, -35e-4, 0, -5, 0, 0, 1e-2, 4.25879793e-4);
+
+    //sys.addBody(-30e-5, 35e-5, 0, 0, 5, 0, 1e-2, 4.25879793e-5);
+    //sys.addBody(30e-5, -35e-5, 0, 2, 2, 0, 1e-2, 4.25879793e-5);
+    //sys.addBody(30e-5, 35e-5, 0, 5, 0, 0, 1e-2, 4.25879793e-5);
+    //sys.addBody(-30e-5, -35e-5, 0, -5, 0, 0, 1e-2, 4.25879793e-5);
+
+    //sys.addBody(0, 0, 0, 0, 0, 0, 1, 0.00465479256);
+    // sys.addBody(1, 0, 0, 0, 1, 0, 1e-8, 4.25879793e-5);
+
+    //std::cerr << "N-body simulation\n";
+    //std::cerr << "Number of bodies: " << sys.size() << '\n';
+
+
+    //std::cerr << "Survived init" << std::endl;
+
+    let mut i: f64 = 0.0;
+    while i < 100. * PI * 2. as f64 * 2.0 * PI { 
+        //clear();
+
+        for (c, p) in sys.state.iter().enumerate() {
+            g.draw_point(p.state.0.0, p.state.0.1, '.' as u64, (c % 4) as i16);
+        }
+
+        sys.rk4(h);
+
+        for (c, p) in sys.state.iter().enumerate() {
+            g.draw_point(p.state.0.0, p.state.0.1, 'o' as u64, (c % 4) as i16);
+        }
+
+        //double totalE = sys.computeEnergy();
+
+        //g.print(0, 0, )
+        
+        // mvprintw(0, 0, "Energy = %e", totalE);
+        let msg = format!("Time = {:.3e}", i);
+        g.print(0, 0, &msg[..], graphics::WHITE_ON_BLACK);
+        let v0: data::data::basic::Velocity = sys.get_velocity(0);
+        let msg = format!("Velocity 1 = <{:.5e}, {:.5e}, {:.5e}>", v0.0, v0.1, v0.2);
+        g.print(0, 1, &msg[..], graphics::WHITE_ON_BLACK);
+
+        let v1: data::data::basic::Velocity = sys.get_velocity(1);
+        let msg2 = format!("Velocity 2 = <{:.5e}, {:.5e}, {:.5e}>", v1.0, v1.1, v1.2);
+        //mvprintw(3, 0, "v 1 = <%e, %e, %e>", v0.x, v0.y, v0.z);
+        g.print(0, 2, &msg2[..], graphics::WHITE_ON_BLACK);
+
+        g.print(0, g.get_max_y() - 1, "Press ^C to close", graphics::WHITE_ON_BLACK);
+
+        g.refresh();
+
+        // int c = g.sleepInterruptible(1); // returns 0 on no char typed
+
+        // if (c == 'd') {
+        //     g.setScreenRelNoThrow(1);
+        // }
+        // else if (c == 'a') {
+        //     g.setScreenRelNoThrow(-1);
+        // }
+        // else if (c > 0) {
+        //     std::cerr << "c = " << c << '\n';
+        //     break; // close program if any key is hit
+        // }
+
+        graphics::sleep(100);
+
+        i += h;
+    }
+
+    //std::cerr << "Survived run\n";
+
+    g.end();
+}
+
 fn main() {
     println!("Hello, world!");
 
-    test_group_5_graphics(2);
-    
+    //test_group_5_graphics(2);
+    // test_group_3_rk4_1();
+    //test_group_6_collision();
+    collision(1e-15, 0.0);
     //test_group_2_kick_step_1();
 
 }
